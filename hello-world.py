ab7e2240f63f3d438ee195404aca0dd6eed2e4e6
Thomas Binétruy <tbinetruy@gmail.com>
2020-11-18 13:58:43 +0100

    My first commit

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+bar = {'a': [i for i in range(len([1, 2, 3])) if i > 1], 'b': "some string", 'c': "some final comments", 'd': "myself as well"}
+
+
+def test():
+    foo = np.array([1, 2, 3])
+    return 0
